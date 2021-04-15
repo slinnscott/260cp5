20be8517817153fe59cf64c00d42bbe54ce6c141
slinnscott <sdslinn@gmail.com>
2021-04-14 23:03:33 -0600

    yay

--- back-end/server.js
+++ back-end/server.js
@@ -14,5 +14,59 @@ mongoose.connect('mongodb://localhost:27017/museum', {
   useNewUrlParser: true
 });
 
+// Create a new item in the museum: takes a title and a path to an image.
+app.post('/api/items', async (req, res) => {
+  const item = new Item({
+    title: req.body.title,
+    path: req.body.path,
+  });
+  try {
+    await item.save();
+    res.send(item);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+// Get a list of all of the items in the museum.
+app.get('/api/items', async (req, res) => {
+  try {
+    let items = await Item.find();
+    res.send(items);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+// Configure multer so that it will upload to '../front-end/public/images'
+const multer = require('multer')
+const upload = multer({
+  dest: '../front-end/public/images/',
+  limits: {
+    fileSize: 10000000
+  }
+});
+
+// Create a scheme for items in the museum: a title and a path to an image.
+const itemSchema = new mongoose.Schema({
+  title: String,
+  path: String,
+});
+
+app.post('/api/photos', upload.single('photo'), async (req, res) => {
+  // Just a safety check
+  if (!req.file) {
+    return res.sendStatus(400);
+  }
+  res.send({
+    path: "/images/" + req.file.filename
+  });
+});
+
+// Create a model for items in the museum.
+const Item = mongoose.model('Item', itemSchema);
+
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
